803e3efc7d91e296782dd0dcda8da002657349c6
Ragielfaqih <ragielfaqih1109@gmail.com>
2023-11-17 21:06:01 +0700

    revisi pencatatan

---
+++ app/src/main/java/com/rival/tutorialloginregist/penyiramanadd.kt
@@ -0,0 +1,13 @@
+package com.rival.tutorialloginregist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class penyiramanadd : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_penyiramanadd)
+
+    }
+}
